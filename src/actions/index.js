239609e7c60dfb50db3ea0b622c91a02e5670e2c
htt12 <harrisontruong2@gmail.com>
2018-04-06 15:36:03 -0700

    Completed getting list data from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,15 @@
 
-import Types from './types';
+import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=harry1234';
+
+export function getList(){
+    const response = axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+    return {
+        type: types.GET_LIST_DATA,
+        payload: response,
+    }
+}
