c45c62ea9ee7eac29621ded5390a05a85dae4025
htt12 <harrisontruong2@gmail.com>
2018-04-09 12:24:22 -0700

    View single item complete

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -3,15 +3,19 @@ import types from '../actions/types';
 //Contains the state. State of reducers do not have to be objects
 const DEFAULT_STATE = {
     items: [],
+    singleItem: {},
     
 };
 
 export default function (state = DEFAULT_STATE, action){
-    debugger;
+    
     switch (action.type){
         case types.GET_LIST_DATA:
             console.log('LIST REDUCER - GET LIST', action.payload)
             return {...state, items: action.payload.data.todos};
+        case types.GET_SINGLE_ITEM:
+            console.log('Get sing item result: ', action);
+            return{...state, singleItem: action.payload.data.todo}
         default:
             return state;
     }
